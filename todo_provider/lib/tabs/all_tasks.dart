55540a792603b0d2ecdffb095cd6eeb762f2ac1c
Sunchit <lakhanpalsunchit8@gmail.com>
2020-10-01 00:52:55 +0530

    ToDo App using Provider State Management

---
+++ todo_provider/lib/tabs/all_tasks.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_provider/models/task.dart';
+import 'package:todo_provider/providers/todos_model.dart';
+import 'package:todo_provider/widgets/task_list.dart';
+
+class AllTasksTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Consumer<TodosModel>(
+        builder: (context, todos, child) => TaskList(
+          tasks: todos.allTasks,
+        ),
+      ),
+    );
+  }
+}
---
+++ todo_provider/lib/tabs/complete_tasks.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_provider/providers/todos_model.dart';
+import 'package:todo_provider/widgets/task_list.dart';
+
+class CompletedTasksTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Consumer<TodosModel>(
+        builder: (context, todos, child) => TaskList(
+          tasks: todos.completedTasks,
+        ),
+      ),
+    );
+  }
+}
