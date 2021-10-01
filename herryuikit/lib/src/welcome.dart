65f2069711adae844364cc1db56ee88552f3dfae
herry88 <herry081288@gmail.com>
2021-10-02 02:42:05 +0700

    buildwelcomePage

---
+++ herryuikit/lib/src/welcome.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
