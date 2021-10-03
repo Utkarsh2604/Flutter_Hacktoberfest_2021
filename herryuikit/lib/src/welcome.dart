5d639c3828e014c79120c18069822d2af80a5c15
herry88 <herry081288@gmail.com>
2021-10-03 10:42:39 +0700

    addnew

--- herryuikit/lib/src/welcome.dart
+++ herryuikit/lib/src/welcome.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 
 class WelcomePage extends StatelessWidget {
-  const WelcomePage({ Key? key }) : super(key: key);
+  final Widget? title;
+  const WelcomePage({ Key? key, this.title }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text('Belajar'),
+      ),
     );
   }
 }
